c838e0a1995b478028c7f5b9f94212ff96f9e133
José Calumbo <79815125+JoseCalumbo@users.noreply.github.com>
2022-02-22 02:21:23 +0100

    nova alteracao
    senario

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,12 @@
 
+var a = document.querySelector('.prev')
+var img = document.querySelector('.item-slides')
+
+a.addEventListener('click',clicar)
+
+function clicar(){
+   img.style.display = 'block'
+
+}
+
 
